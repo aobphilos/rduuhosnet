edbe59641831cf9ff1e910090fc7607d6fbbc2dd
Batman @Moshi-W8 <aobphilos@gmail.com>
2016-06-28 08:01:58 +0700

    complete first section, add owl-carousel

--- assets/js/app/controllers/home.controller.js
+++ assets/js/app/controllers/home.controller.js
@@ -5,15 +5,48 @@
         .module('rdu.ui')
         .controller('HomeCtrl', [
             '$rootScope',
+            '$timeout',
             HomeCtrl
         ]);
 
-    function HomeCtrl($rootScope) {
+    function HomeCtrl($rootScope, $timeout) {
         var vm = this;
 
         init();
 
-        function init() {}
+        function initCarousel() {
+            $('.owl-carousel').owlCarousel({
+                autoplay: true,
+                autoWith: true,
+                center: true,
+                loop: true,
+                margin: 10,
+                nav: false,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        center: false
+                    },
+                    600: {
+                        items: 2,
+                        center: false,
+                    },
+                    960: {
+                        items: 3
+                    }
+                }
+            });
+        }
+
+        function init() {
+
+            angular.element(document).ready(function() {
+                $timeout(initCarousel, 100);
+            });
+
+        }
+
 
     }
 
