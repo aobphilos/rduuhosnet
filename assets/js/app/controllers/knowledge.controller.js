1aa5245ac9fc8fbfe12e82590bd1221e8448aebd
Batman @ZiiOS <aobphilos@gmail.com>
2016-07-15 18:43:50 +0700

    update healthcare page

--- assets/js/app/controllers/knowledge.controller.js
+++ assets/js/app/controllers/knowledge.controller.js
@@ -1,104 +1,151 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    angular
-        .module('rdu.ui.knowledge')
-        .controller('PeopleCtrl', [
-            '$rootScope',
-            '$window',
-            '_',
-            PeopleCtrl
-        ])
-        .controller('HealthCareCtrl', [
-            '$rootScope',
-            '$window',
-            '_',
-            HealthCareCtrl
-        ]);
+  angular
+    .module('rdu.ui.knowledge')
+    .controller('KnowledgePeopleCtrl', [
+      '$rootScope',
+      '$window',
+      '_',
+      KnowledgePeopleCtrl
+    ])
+    .controller('KnowledgeHealthCareCtrl', [
+      '$rootScope',
+      '$window',
+      '$location',
+      '_',
+      KnowledgeHealthCareCtrl
+    ])
+    .controller('KnowledgeHealthcareLabelCtrl', [
+      '$rootScope',
+      '_',
+      KnowledgeHealthcareLabelCtrl
+    ]);
 
-    function PeopleCtrl($rootScope, $window, _) {
-        var vm = this;
+  function KnowledgePeopleCtrl($rootScope, $window, _) {
+    var vm = this;
 
-        vm.images = [{
-            name: "geriatric",
-            image: "/images/knowledge/people/geriatric.jpg"
-        }, {
-            name: "kid",
-            image: "/images/knowledge/people/kid.jpg"
-        }, {
-            name: "lactation",
-            image: "/images/knowledge/people/lactation.jpg"
-        }, {
-            name: "liver",
-            image: "/images/knowledge/people/liver.jpg"
-        }, {
-            name: "preg",
-            image: "/images/knowledge/people/preg.jpg"
-        }, {
-            name: "renal",
-            image: "/images/knowledge/people/renal.jpg"
-        }];
+    vm.images = [
+      {
+        name: "geriatric",
+        image: "/images/knowledge/people/geriatric.jpg"
+      },
+      {
+        name: "kid",
+        image: "/images/knowledge/people/kid.jpg"
+      },
+      {
+        name: "lactation",
+        image: "/images/knowledge/people/lactation.jpg"
+      },
+      {
+        name: "liver",
+        image: "/images/knowledge/people/liver.jpg"
+      },
+      {
+        name: "preg",
+        image: "/images/knowledge/people/preg.jpg"
+      },
+      {
+        name: "renal",
+        image: "/images/knowledge/people/renal.jpg"
+      }
+    ];
 
-        vm.showImage = showImage;
-        vm.navigate = navigate;
+    vm.showImage = showImage;
+    vm.navigate = navigate;
 
-        init();
+    init();
 
-        function showImage(name) {
+    function showImage(name) {
+      var image = _.filter(vm.images, function (s) {
+        return s.name === name;
+      });
 
-            var image = _.filter(vm.images, function(s) {
-                return s.name === name;
-            });
+      if (image.length > 0)
+        vm.image = image[0]["image"];
+    }
 
-            if (image.length > 0)
-                vm.image = image[0]["image"];
+    function navigate(name) {
+      var image = _.filter(vm.images, function (s) {
+        return s.name === name;
+      });
 
-        }
-
-        function navigate(name) {
-
-            var image = _.filter(vm.images, function(s) {
-                return s.name === name;
-            });
-
-            if (image.length > 0)
-                $window.open(image[0]["image"], "knowledge_people");
-
-        }
-
-        function init() {
-
-            vm.showImage("kid");
-
-        }
+      if (image.length > 0)
+        $window.open(image[0]["image"], "knowledge_people");
 
     }
 
-    function HealthCareCtrl($rootScope, $window, _) {
-        var vm = this;
-
-        vm.images = [{
-            name: "MedWatch",
-            image: "/images/knowledge/healthcare/h1.jpg"
-        }, {
-            name: "Ethics",
-            image: "/images/knowledge/healthcare/h2.jpg"
-        }, {
-            name: "Labeling",
-            image: "/images/knowledge/healthcare/h3.jpg"
-        }, {
-            name: "Update",
-            image: "/images/knowledge/healthcare/h4.jpg"
-        }];
-
-
-        init();
-
-        function init() {
-
-
-
-        }
+    function  init() {
+      vm.showImage('kid');
     }
 
-}());
+  }
+
+  function KnowledgeHealthCareCtrl($rootScope, $window, $location, _) {
+
+    var vm = this;
+
+    const LINK_TYPE = {
+      NONE: 0,
+      LINK: 1,
+      PAGE: 2
+    }
+
+    vm.items = [
+      {
+        name: "MedWatch",
+        image: "/images/knowledge/healthcare/h1.jpg",
+        link: "http://www.fda.gov/Safety/MedWatch/",
+        linkType: LINK_TYPE.LINK
+      },
+      {
+        name: "Ethics",
+        image: "/images/knowledge/healthcare/h2.jpg",
+        link: "",
+        linkType: LINK_TYPE.NONE
+      },
+      {
+        name: "Labeling",
+        image: "/images/knowledge/healthcare/h3.jpg",
+        link: "/knowledge/healthcare/label",
+        linkType: LINK_TYPE.PAGE
+      },
+      {
+        name: "Update",
+        image: "/images/knowledge/healthcare/h4.jpg",
+        link: "",
+        linkType: LINK_TYPE.NONE
+      }
+    ];
+
+    vm.navigate = navigate;
+
+    init();
+
+    function navigate(item) {
+
+      switch (item.linkType) {
+
+        case LINK_TYPE.LINK:
+          $window.open(item.link, "knowledge_healthcare");
+          break;
+
+        case LINK_TYPE.PAGE:
+          $location.path(item.link);
+          break;
+
+        default: break;
+      }
+
+    }
+
+    function init() {
+
+    }
+  }
+
+  function KnowledgeHealthcareLabelCtrl($rootScope, _) { }
+
+} ());
+
