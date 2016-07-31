9a08263c7fcbe186a42cccba67aa52beed349396
Batman @Moshi-W10 <aobphilos@gmail.com>
2016-08-01 04:47:25 +0700

    add member page, add authentication suite, add dummy user

--- assets/js/app/controllers/member.controller.js
+++ assets/js/app/controllers/member.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -8,18 +8,69 @@
             '$window',
             '$timeout',
             '_',
+            'AuthApi',
+            '$mdDialog',
             MemberCtrl
         ]);
 
-    function MemberCtrl($rootScope, $window, $timeout, _) {
+    function MemberCtrl($rootScope, $window, $timeout, _, AuthApi, $mdDialog) {
         var vm = this;
 
-        init();
-       
-        function init() {
+        vm.isAuthenticated = false;
+        vm.username = "";
+        vm.password = "";
 
+        vm.login = login;
+        vm.inputKeyDown = inputKeyDown;
+        vm.showAlert = showAlert;
+
+        init();
+
+        function inputKeyDown(e, isValid) {
+            if (e.keyCode === 13) {
+                vm.login(isValid);
+            }
+        }
+
+        function checkAuthen() {
+            AuthApi.isAuthen(null).$promise
+                .then(function (result) {
+                    vm.isAuthenticated = result.isAuthen;
+                });
+        }
+
+        function login(isValid) {
+            
+            if (isValid) {
+                AuthApi.login({ username: vm.username, password: vm.password }).$promise
+                    .then(function (result) {
+                        if (result.user) {
+                            vm.isAuthenticated = true;
+                        } else {
+                            vm.isAuthenticated = false;
+                            vm.showAlert(result.message);
+
+                        }
+                    });
+            }
+        }
+
+        function showAlert(msg) {
+            alert = $mdDialog.alert()
+                .title('Login Failed')
+                .textContent(msg)
+                .ok('Close');
+            $mdDialog
+                .show(alert)
+                .finally(function () {
+                    alert = undefined;
+                });
+        }
+
+        function init() {
+            checkAuthen();
         }
 
     }
 
-}());
+} ());
---
+++ assets/js/app/factories/auth.factory.js
@@ -0,0 +1,25 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('rdu.ui')
+          .factory('AuthApi', ['$resource', adapter])
+
+    function adapter($resource) {
+        return $resource('/api/login', null, {
+            "login": {
+                method: 'POST',
+                url: '/api/login'
+            },
+            "logout": {
+                method: 'GET',
+                url: '/api/logout'
+            },
+            "isAuthen": {
+                method: 'GET',
+                url: '/api/isauthen'
+            }
+        });
+    }
+
+}());
