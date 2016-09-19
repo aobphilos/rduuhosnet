abb4a20a53a0214e8f1fef01129c150c597245ea
Batman @Moshi-W10 <aobphilos@gmail.com>
2016-09-20 01:23:44 +0700

    add SendGrid Api and MailService

---
+++ config/email.js
@@ -0,0 +1,10 @@
+module.exports.email = {
+
+  from: "rduhos@gmail.com",
+  fromName: "RDU Web",
+  subject: "Please confirm your email address",
+  template: './api/data/mail.template.html',
+  icon: 'https://www.dropbox.com/s/vwtk70dhlc5kehx/logo.png?dl=0',
+  homePage: 'http://rduuhosnet.herokuapp.com/'
+
+};
